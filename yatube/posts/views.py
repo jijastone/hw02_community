be3ec5a8f401dc6b4eb773c709bc2bcb9c17c54f
Михаил Копанев <mkopanev28@gmail.com>
2023-04-12 21:08:09 +0500

    begin

---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from .models import Post, Group
+
+
+# Главная страница
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
